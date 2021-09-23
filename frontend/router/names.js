e126b798cedbaf00a718a391b8d825b238efd02f
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-09-23 13:41:37 +0200

    fix: Router names globalized

---
+++ frontend/router/names.js
@@ -0,0 +1,7 @@
+const routeNames = {
+  PAGE_HOME: 'Home',
+  PAGE_BACON: 'Bacon',
+  PAGE_CHECKOUT: 'Checkout',
+}
+
+export default routeNames
--- frontend/router/router.js
+++ frontend/router/router.js
@@ -1,12 +1,27 @@
 import VueRouter from 'vue-router'
+import routeNames from './names.js'
 
 const routes = [
-  { path: '/', component: () => import(/* webpackChunkName: "pages-index" */ '../pages/Index.vue') },
-  { path: '/bacon', component: () => import(/* webpackChunkName: "pages-bacon" */ '../pages/Bacon.vue') },
-  { path: '/checkout', component: () => import(/* webpackChunkName: "pages-checkout" */ '../pages/Checkout.vue') },
+  {
+    path: '/',
+    name: routeNames.PAGE_HOME,
+    component: () => import(/* webpackChunkName: "pages-index" */ '../pages/Index.vue'),
+  },
+  {
+    path: '/bacon',
+    name: routeNames.PAGE_BACON,
+    component: () => import(/* webpackChunkName: "pages-bacon" */ '../pages/Bacon.vue'),
+  },
+  {
+    path: '/checkout',
+    name: routeNames.PAGE_CHECKOUT,
+    component: () => import(/* webpackChunkName: "pages-checkout" */ '../pages/Checkout.vue'),
+  },
 ]
 
 export default new VueRouter({
   mode: 'history',
   routes,
 })
+
+export { routeNames }
