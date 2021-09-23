f5b808f1895fe040bb324062d63a119010df6c67
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-09-23 09:00:32 +0200

    feat: added comment on introducting Vue to project

--- frontend/main.js
+++ frontend/main.js
@@ -2,6 +2,15 @@ import './assets/fonts.css'
 import './assets/variables.css'
 import './assets/app.css'
 
+/**
+ * Introducting Vue comes with the cost of losing SSR.
+ * As long as this is recruitment task this is okey
+ * 
+ * If I can make an educated guess, maybe
+ * Puppeteer + SSG straight to the *.edge files
+ * will be an answer? As always, it depends! ;)
+ */
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueMeta from 'vue-meta'
