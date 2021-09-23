ec90967dc4bcd2cde8a995103316d3e775757142
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-09-23 17:50:58 +0200

    feat: Checkout component draft

--- frontend/pages/Checkout.vue
+++ frontend/pages/Checkout.vue
@@ -1,18 +1,29 @@
 <template>
   <BaseArticle class="pages-checkout">
-    Checkout
+    <section class="pages-checkout__checkout-steps">
+      Steps
+    </section>
+    <Summary
+      class="pages-checkout__summary"
+      :cart="cart"
+    />
   </BaseArticle>
 </template>
 
 <script>
+import Summary from '../components/checkout/Summary/Summary.vue'
+
 export default {
   name: 'pagesCheckout',
   metaInfo: {
     title: 'Checkout',
   },
-  data() {
-    return {
-      cart: {
+  components: {
+    Summary,
+  },
+  computed: {
+    cart() {
+      return {
         items: [
           { name: 'Apple Watch Sport', price: 580 },
           { name: 'Modern Buckle', price: 380 },
@@ -22,8 +33,38 @@ export default {
           tax: 0,
           grandTotal: 960,
         },
-      },
+      };
     }
-  }
+  },
 }
 </script>
+
+<style scoped>
+.pages-checkout {
+  background-color: var(--checkout-background-color);
+  display: grid;
+  gap: 38px 12px;
+  grid-template: 
+    "summary" 1fr
+    "checkout-steps" 1fr
+      / 1fr;
+  font-family: var(--checkout-font-family);
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 48px;
+}
+
+@media screen and (min-width: 768px) {
+  .pages-checkout {
+    grid-template: "checkout-steps summary" 1fr / 2fr 1fr;
+  }
+}
+
+.pages-checkout__checkout-steps {
+  grid-area: checkout-steps;
+}
+
+.pages-checkout__summary {
+  grid-area: summary;
+}
+</style>
