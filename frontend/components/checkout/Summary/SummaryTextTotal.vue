226e4e539d1bb2f8b0bf3befc5ae647251300a62
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2021-09-23 17:49:18 +0200

    feat: SummaryTextTotal component

---
+++ frontend/components/checkout/Summary/SummaryTextTotal.vue
@@ -0,0 +1,21 @@
+<script>
+import SummaryText from './SummaryText.vue'
+
+export default {
+  name: "SummaryTextTotal",
+  extends: SummaryText,
+}
+</script>
+
+<style scoped>
+@import url("./SummaryText.css");
+
+.summary-text {
+  grid-template-columns: 1fr 100px;
+  font-weight: bold;
+}
+
+.summary-text__value {
+  font-size: 2em;
+}
+</style>
